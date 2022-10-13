a239aeb695ff87fac4260ea587c693ceb596a77b
Геннадий Бакулин <g.bakulin@yandex.ru>
2022-10-13 21:56:13 +0700

    checking the operation of plugins 2

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { Dropdowns } from "./shared/Dropdown/Dropdown";
 
 export default function App() {
   return (
-              <div>
+                   <div>
       <div className="text-day-night">
         Input
             <Inputs />
