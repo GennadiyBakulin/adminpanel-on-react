3abdd5dc3ae38c216521bbf3d201e4300dd557ca
Геннадий Бакулин <g.bakulin@yandex.ru>
2022-11-01 21:59:36 +0700

    settings es-lint and prettier

--- src/modules/Table/Table.jsx
+++ src/modules/Table/Table.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import styles from "./Table.module.css";
-import {TableHeader} from "./TableHeader/TableHeader";
-import {TableBody} from "./TableBody/TableBody";
-import {TableFooter} from "./TableFooter/TableFooter";
+import React from 'react';
+import styles from './Table.module.css';
+import { TableHeader } from './TableHeader/TableHeader';
+import { TableBody } from './TableBody/TableBody';
+import { TableFooter } from './TableFooter/TableFooter';
 
 export const Table = () => {
-  return (
-    <div className={styles._}>
-      <TableHeader />
-      <TableBody />
-      <TableFooter />
-    </div>
-  );
+	return (
+		<div className={styles._}>
+			<TableHeader />
+			<TableBody />
+			<TableFooter />
+		</div>
+	);
 };
