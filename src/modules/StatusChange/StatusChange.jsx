89765da0a66a2178a1d6d337aec724ada7d40aef
Геннадий Бакулин <g.bakulin@yandex.ru>
2022-11-11 20:30:53 +0700

    сделал Dropdown изменить статус в футере таблицы

--- src/modules/StatusChange/StatusChange.jsx
+++ src/modules/StatusChange/StatusChange.jsx
@@ -1,13 +1,66 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './StatusChange.module.css';
-import { Button } from '../../shared/components';
+import { Button, Dropdown, Radio } from '../../shared/components';
+import { FiltersContext } from '../../contexts/FiltersContext';
 
 export const StatusChange = () => {
+  const { STATUSES_NAMES_TRANSLATION } = useContext(FiltersContext);
   return (
-    <div className={styles._}>
-      <Button theme='blue' size='small' icon='pencil'>
-        Изменить статус
-      </Button>
-    </div>
+    <Dropdown
+      className={styles._}
+      trigger={
+        <div>
+          <Button theme='blue' size='small' icon='pencil'>
+            Изменить статус
+          </Button>
+        </div>
+      }
+      overlay={
+        <>
+          <Radio
+            key='new'
+            className={styles.radio}
+            name='statusChangeRadio'
+            value='new'
+            text={STATUSES_NAMES_TRANSLATION.new}
+          />
+          <Radio
+            className={styles.radio}
+            text={STATUSES_NAMES_TRANSLATION.calculated}
+            key='calculating'
+            name='statusChangeRadio'
+            value='calculating'
+          />
+          <Radio
+            className={styles.radio}
+            text={STATUSES_NAMES_TRANSLATION.confirmed}
+            value='confirm'
+            key='confirm'
+            name='statusChangeRadio'
+          />
+          <Radio
+            className={styles.radio}
+            text={STATUSES_NAMES_TRANSLATION.postponed}
+            value='postponed'
+            key='postponed'
+            name='statusChangeRadio'
+          />
+          <Radio
+            className={styles.radio}
+            text={STATUSES_NAMES_TRANSLATION.completed}
+            value='done'
+            key='done'
+            name='statusChangeRadio'
+          />
+          <Radio
+            className={styles.radio}
+            text={STATUSES_NAMES_TRANSLATION.cancelled}
+            value='canceled'
+            key='canceled'
+            name='statusChangeRadio'
+          />
+        </>
+      }
+    />
   );
 };
--- src/shared/components/Radio/Radio.jsx
+++ src/shared/components/Radio/Radio.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import classnames from 'classnames';
 import styles from './Radio.module.css';
 
-export const Radio = function Radio({ className, name, text, checked }) {
+export const Radio = function Radio({ className, name, value, text, checked }) {
   const blockClass = classnames(styles._, className);
 
   return (
     <label className={blockClass}>
-      <input className={styles.area} type='radio' name={name} checked={checked} />
+      <input className={styles.area} type='radio' name={name} value={value} checked={checked} />
       {text && <span className={styles.text}>{text}</span>}
     </label>
   );
