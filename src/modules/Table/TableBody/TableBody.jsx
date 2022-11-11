8585af85ea47fe3dc32748806c86ffaf6b69f264
Геннадий Бакулин <g.bakulin@yandex.ru>
2022-11-11 20:54:59 +0700

    правка сортировки по полю Статус

--- src/modules/Table/TableBody/TableBody.jsx
+++ src/modules/Table/TableBody/TableBody.jsx
@@ -6,16 +6,24 @@ import { TableRow } from '../TableRow/TableRow';
 import { StatusOrder } from '../StatusOrder/StatusOrder';
 import { ordersMock } from '../../mocks/ordersMock100Records';
 import { SortedContext } from '../../../contexts/SortedContext';
+import { FiltersContext } from '../../../contexts/FiltersContext';
 
 let srt = ordersMock.concat();
 export const TableBody = () => {
+  const { STATUSES_NAMES_TRANSLATION } = useContext(FiltersContext);
   const { columnSorted, directionSorted } = useContext(SortedContext);
   if (directionSorted) {
     srt = srt.sort((a, b) => {
+      if (columnSorted === 'status') {
+        return STATUSES_NAMES_TRANSLATION[a[columnSorted]] > STATUSES_NAMES_TRANSLATION[b[columnSorted]] ? 1 : -1;
+      }
       return a[columnSorted] > b[columnSorted] ? 1 : -1;
     });
   } else {
     srt = srt.sort((a, b) => {
+      if (columnSorted === 'status') {
+        return STATUSES_NAMES_TRANSLATION[a[columnSorted]] < STATUSES_NAMES_TRANSLATION[b[columnSorted]] ? 1 : -1;
+      }
       return a[columnSorted] < b[columnSorted] ? 1 : -1;
     });
   }
